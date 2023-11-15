810aac869748e85f07742636c69425e0f676c9c5
MicaiahHtml <mvinson289@gmail.com>
2023-11-15 14:39:53 -0500

    fixed LuhnAlg.js to be accurate for all card nums
    Before, the sum was weird and it only worked for the homework problems, causing me to think I was good and submit the homework prematurely ☹️

--- src/components/LuhnAlg.js
+++ src/components/LuhnAlg.js
@@ -15,9 +15,15 @@ export default function LuhnAlg(cardNum) {
   for (let i = cardArr.length - 2; i >= 0; i -= 2) {
     cardArr[i] *= 2;
   }
+
+  let tempString = cardArr.toString().replace(/,/g, "");
+  cardArr = tempString.split("");
+  console.log("Before: " + cardArr);
+
   cardArr.forEach((num) => {
-    cardSum += num;
+    cardSum += parseInt(num);
   });
+  console.log("After: " + cardSum);
   //alert(cardSum % 10);
   if (cardSum % 10 == 0) {
     return true;
